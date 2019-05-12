4a05b991ad7f1a0282e309058ea89b5b9e1f2721
Shigeo Nakamura <info@shigebox.com>
2019-05-13 00:52:04 +0900

    ボリュームマウントしたフォルダの中を削除します。
    通常、Linux の docker は、マウント先のフォルダを作成しますが、
    Windows の docker はマウント先のフォルダ存在しないといけないので、
    本バッチファイルを Windows docker でも流用することを念頭において、
    フォルダの中味を削除する実装にしています。

---
+++ clear-volume.bat
@@ -0,0 +1,20 @@
+@echo off
+
+if "%1" == "-f" goto yes
+if "%1" == "-F" goto yes
+
+set /P ANSWER="ボリュームマウントで作成されたファイルを削除します (Y/N)？"
+
+if /i {%ANSWER%}=={y} (goto :yes)
+if /i {%ANSWER%}=={yes} (goto :yes)
+
+goto :end
+
+:yes
+
+set TARGET=redmine\*
+
+del /S /Q %TARGET%
+for /D %%1 in (%TARGET%) do echo 削除したディレクトリ - %%1 & rmdir /S /Q %%1
+
+:end
